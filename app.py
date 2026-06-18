7f6b0f9c57fb557b008d0f242a7d0da1b97c03ab
clemndy <clement.naudy@edu.devinci.fr>
2026-06-18 10:41:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ dico_indicateurs = {
             "Capacité de désendettement (années)", "Crédits de trésorerie", "Dépôts au Trésor"
         ]
     },
-    "caca boudin" : { "indicatos : ["Annuité de la dette", "Charges financières"] }
+    "caca boudin": { "indicatos" : ["Annuité de la dette", "Charges financières"] }
     
 }
 
